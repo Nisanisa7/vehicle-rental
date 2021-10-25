3f89698ec209fc99eda38b1644237e7e57143f99
unknown <bakamegane74@gmail.com>
2021-10-25 11:56:27 +0700

    adding trycatch to logout

--- pages/api/logoutadmin.js
+++ pages/api/logoutadmin.js
@@ -1,26 +1,32 @@
 
 const Logoutadmin = (req, res) => {
-  if (req.method === "GET") {
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
-  res.setHeader('Set-Cookie', [
-    serialize('token', '', {
-      maxAge: -1,
-      path: '/',
-    }),
-    serialize('user_isAuth', '', {
-      maxAge: -1,
-      path: '/',
-    }),
-    serialize('user_idAdmin', '', {
-      maxAge: -1,
-      path: '/',
-    }),
-  ]);
-  res.status(200)
-  res.json({message:'logout success'})
-}
+  try {
+    if (req.method === "GET") {
+      res.setHeader("Access-Control-Allow-Headers", "*");
+      res.setHeader("Access-Control-Allow-Credentials", true);
+      res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+      res.setHeader('Set-Cookie', [
+        serialize('token', '', {
+          maxAge: -1,
+          path: '/',
+        }),
+        serialize('user_isAuth', '', {
+          maxAge: -1,
+          path: '/',
+        }),
+        serialize('user_idAdmin', '', {
+          maxAge: -1,
+          path: '/',
+        }),
+      ]);
+      res.status(200)
+      res.json({message:'logout success'})
+    }
+  } catch (error) {
+    res.status(error.response.status)
+    res.json({data: null, error: {message: error.response}})
+  }
+ 
 }
 
 export default Logoutadmin
