8f46164440deb7a885bbf8662bd530c26df5b0a2
unknown <bakamegane74@gmail.com>
2021-10-25 12:21:35 +0700

    change logout againnn

--- pages/api/logoutadmin.js
+++ pages/api/logoutadmin.js
@@ -1,30 +1,40 @@
-
+import cookie from "cookie";
 const Logoutadmin = (req, res) => {
   try {
     if (req.method === "POST") {
       res.setHeader("Access-Control-Allow-Headers", "*");
       res.setHeader("Access-Control-Allow-Credentials", true);
       res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
-      res.setHeader('Set-Cookie', [
-        serialize('token', '', {
+      res.setHeader(
+        "Set-Cookie",
+        [cookie.serialize("token", result.token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "strict",
           maxAge: -1,
-          path: '/',
+          path: "/", 
         }),
-        serialize('user_isAuth', '', {
+        cookie.serialize("user_isAuth", true, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "strict",
           maxAge: -1,
-          path: '/',
-        }),
-        serialize('user_idAdmin', '', {
-          maxAge: -1,
-          path: '/',
-        }),
-      ]);
+          path: "/",
+        }),  
+          cookie.serialize("user_idAdmin", result.idAdmin, {
+             httpOnly: true,
+             secure: true,
+             sameSite: "strict",
+             maxAge: -1,
+             path: "/",
+           }),       
+        ])
       res.status(200)
       res.json({message:'logout success'})
     }
   } catch (error) {
     res.status(error.response.status)
-    res.json({data: null, error: {message: error.response}})
+    res.json({data: null, error: "error logout"})
   }
  
 }
